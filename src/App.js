57f8eec6f4374e72f734609dc273bec90eddfb15
Oksana Moskviak <84397218+okqsna@users.noreply.github.com>
2023-04-25 17:29:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import HomePage from './Components/HomePage/HomePage';
+import AboutPage from './Components/AboutPage/AboutPage';
+import HelpPage from './Components/HelpPage/HelpPage';
 import GalleryPage from './Components/GalleryPage/GalleryPage';
 function App() {
   return (
     <div className="App">
+     <HomePage/>
+     <AboutPage/>
+     <HelpPage/>
      <GalleryPage/>
     </div>
   );
